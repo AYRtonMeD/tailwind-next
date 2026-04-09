bd5f3c8ed0fafae42acaf15f63c0d10d60fada1c
AYRtonMeD <ayrtondantas2013@gmail.com>
2026-04-08 21:01:52 -0300

    feat: adiciona parte inferior da sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,14 +1,17 @@
 import {
   BarChart,
   CheckSquare,
+  Cog,
   Flag,
   Home,
+  LifeBuoy,
   Search,
   SquareStack,
   Users,
 } from "lucide-react";
 import { Logo } from "./Logo";
 import NavItem from "./NaviItem";
+import UsedSpaceWidget from "./UsedSpaceWidget";
 
 export default function Sidebar() {
   return (
@@ -31,6 +34,15 @@ export default function Sidebar() {
         <NavItem title="Reporting" icon={Flag} />
         <NavItem title="Users" icon={Users} />
       </nav>
+
+      <div className="mt-auto flex flex-col gap-6">
+        <nav className="space-y-0.5">
+          <NavItem title="Support" icon={LifeBuoy} />
+          <NavItem title="Settings" icon={Cog} />
+        </nav>
+
+        <UsedSpaceWidget />
+      </div>
     </aside>
   );
 }
