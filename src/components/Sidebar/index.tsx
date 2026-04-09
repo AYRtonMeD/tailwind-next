f99467988bfe49b57a490ba5accf02f4c4ba2e1c
AYRtonMeD <ayrtondantas2013@gmail.com>
2026-04-08 21:00:28 -0300

    refactor: remove componente e move conteudo para componente pai

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,10 +1,18 @@
-import { Search } from "lucide-react";
+import {
+  BarChart,
+  CheckSquare,
+  Flag,
+  Home,
+  Search,
+  SquareStack,
+  Users,
+} from "lucide-react";
 import { Logo } from "./Logo";
 import MainNavigation from "./MainNavigation";
 
 export default function Sidebar() {
   return (
-    <aside className="border-r border-zinc-200 px-5 py-8 space-y-6">
+    <aside className="border-r border-zinc-200 px-5 py-8 flex flex-col gap-6">
       <Logo />
 
       <div className="flex mx-1 w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
@@ -15,7 +23,14 @@ export default function Sidebar() {
         />
       </div>
 
-      <MainNavigation />
+      <nav className="space-y-0.5">
+        <NavItem title="Home" icon={Home} />
+        <NavItem title="Dashboard" icon={BarChart} />
+        <NavItem title="Projects" icon={SquareStack} />
+        <NavItem title="Tasks" icon={CheckSquare} />
+        <NavItem title="Reporting" icon={Flag} />
+        <NavItem title="Users" icon={Users} />
+      </nav>
     </aside>
   );
 }
