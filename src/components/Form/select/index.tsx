96aa644564ff072a239fe0b1e0e58f097e47ab44
AYRtonMeD <ayrtondantas2013@gmail.com>
2026-04-20 21:08:55 -0300

    chore: adiciona efeitos de focus nos componentes

--- src/components/Form/select/index.tsx
+++ src/components/Form/select/index.tsx
@@ -19,7 +19,8 @@ export default function Select({
       <SelectPrimitive.Trigger
         className={`flex h-11 w-full items-center justify-between gap-2
                             rounded-lg border border-zinc-300 px-3 py-2 shadow-sm
-                            data-placeholder:text-zinc-600`}
+                            data-placeholder:text-zinc-600 outline-none
+                            focus:border-violet-300 focus:ring-4 focus:ring-violet-100`}
       >
         <SelectPrimitive.Value
           placeholder={placeholder}
--- src/components/SettingsTabs/TabItem.tsx
+++ src/components/SettingsTabs/TabItem.tsx
@@ -17,9 +17,12 @@ export default function TabItem({
   return (
     <Tabs.Trigger
       value={value}
-      className="relative px-1 pb-4 text-sm font-medium text-zinc-500 hover:text-violet-700 hover:cursor-pointer data-[state=active]:text-violet-700"
+      className={`relative px-1 pb-4 text-sm font-medium text-zinc-500 hover:text-violet-700 
+        hover:cursor-pointer data-[state=active]:text-violet-700 outline-none group`}
     >
-      <span>{title}</span>
+      <span className="rounded group-focus-visible:ring-2 group-focus-visible:ring-violet-400 group-focus-visible:ring-offset-4">
+        {title}
+      </span>
 
       {isSelected && (
         <motion.div
